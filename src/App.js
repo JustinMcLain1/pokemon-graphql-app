aef8bb8b980337c1296ff6b820f1c6a91ac72e73
JustinMcLain1 <justinkeithmclain@gmail.com>
2022-08-11 14:50:52 -0500

    written query and start of app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+import React from 'react';
+import ApolloClient from 'apollo-boost'; // default package
+import {ApolloProvider} from '@apollo/react-hooks'; // named import
+
 
 function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.sh'
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <main>
+      <p>I am a pokemon</p>
+    </main>
+    </ApolloProvider>
   );
 }
 
---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+
+export const PokemonsContainer = () => {
+    
+}
